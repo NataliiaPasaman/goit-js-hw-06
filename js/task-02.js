b39aa4df361adfc0b6dfc9343c39566c2ad5e40d
NataliiaPasaman <natapasaman@gmail.com>
2022-08-16 23:01:48 +0300

    script-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const arrayLi = ingredients.map((element) => {
+  const itemElement = document.createElement("li");
+  itemElement.textContent = element;
+  itemElement.classList.add("item");
+  return itemElement;
+});
+
+const listIngredients = document.querySelector("#ingredients");
+listIngredients.append(...arrayLi);
