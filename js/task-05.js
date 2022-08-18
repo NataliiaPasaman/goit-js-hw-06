4fb172c6797f0e09899980f8f01aa5e20a8a9cc0
NataliiaPasaman <natapasaman@gmail.com>
2022-08-19 00:20:23 +0300

    script-5 - fixed

--- js/task-05.js
+++ js/task-05.js
@@ -1,26 +1,13 @@
-/** Напиши скрипт, який під час набору тексту 
- * в інпуті input#name-input (подія input), підставляє
- *  його поточне значення в span#name-output. 
- * Якщо інпут порожній, 
- * у спані повинен відображатися рядок "Anonymous". */
-
 const inputElement = document.querySelector('#name-input');
 const outputElement = document.querySelector('#name-output');
 
 const handleInput = (event) => {
-    console.log(event);
-    console.log(event.currentTarget.value);
-    outputElement.textContent = event.currentTarget.value;
+    const valEl = event.currentTarget.value;
+    outputElement.textContent = valEl;
 
-    // if (inputElement.textContent === "") {
-    //     console.log('ghjk')
-    //     // outputElement.textContent = Anonymous;
-    // }
+    if (valEl === "") {
+        outputElement.textContent = 'Anonymous';
+    }
 }
 
-
-// if (inputElement.textContent === "") {
-//     inputElement.removeEventListener('input', handleInput)
-// }
 inputElement.addEventListener('input', handleInput);
-// inputElement.removeEventListener('input', handleInput);
