04f39a156521c53cdf5ccf9d91e5e9f674808e34
NataliiaPasaman <natapasaman@gmail.com>
2022-08-18 23:11:58 +0300

    script-4 - Counter

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+let counterValue = 0;
+
+const counterElement = document.querySelector('#value');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+
+
+const hendleClicDecrement = (event) => {
+    counterValue -= 1;
+    counterElement.textContent = counterValue;
+    console.log(`Змуншуємо дічильник на 1 - поточне значення ${counterValue}`);
+}
+
+const hendleClicIncrement = (event) => {
+    counterValue += 1;
+    counterElement.textContent = counterValue;
+    console.log(`Збільшуємо дічильник на 1 - поточне значення ${counterValue}`);
+}
+
+decrementBtn.addEventListener('click', hendleClicDecrement);
+incrementBtn.addEventListener('click', hendleClicIncrement);
