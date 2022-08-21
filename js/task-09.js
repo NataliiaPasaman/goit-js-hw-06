cc1a0e25794065dbcc8142a064e122264ec38c78
NataliiaPasaman <natapasaman@gmail.com>
2022-08-21 22:20:35 +0300

    script-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
-function getRandomHexColor() {
+ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyChange = document.querySelector('body');
+const spanRef = document.querySelector('span.color');
+const btnRef = document.querySelector('.change-color');
+
+btnRef.addEventListener('click', function onBtnChangeColor () {
+  bodyChange.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = bodyChange.style.backgroundColor;
+})
