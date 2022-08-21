e50e447ae490fe3f534700573743a53c6dc045b7
NataliiaPasaman <natapasaman@gmail.com>
2022-08-21 21:46:44 +0300

    script-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formElement = document.querySelector('.login-form');
 
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const emailForm = formElements.email.value;
+    const passwordForm = formElements.password.value;
+
+    if (passwordForm === '') {
+        return alert(`Ви не ввели пароль. Будь ласка, заповніть поле Password`);
+    } else if (emailForm === '') {
+        return alert(`Ви не ввели email. Будь ласка, заповніть поле Email`);
+    };
+
+    const formData = { emailForm, passwordForm };
+    console.log(formData);
+    formElement.reset();
+};
+
+formElement.addEventListener('submit', onFormSubmit);
