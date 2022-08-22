8a183a0eb2351ba1af06cb0e356bf5a18b43df06
NataliiaPasaman <natapasaman@gmail.com>
2022-08-22 23:02:41 +0300

    fixed script-1 (ForEach)

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,15 @@
-const countQuantityElementsCategoryWithTitleAndLength = () => {
+const findElementsUl = () => {
 
     const numberCategories = document.querySelectorAll('.item');
     console.log('Number of categories: ' ,numberCategories.length);
 
-    for (const elem of numberCategories) {
+    numberCategories.forEach(elem => {
+        const titleElement = elem.firstElementChild.textContent;
+        const numberItems = elem.children[1].children;
 
-        const textTitleElement = elem.firstElementChild.textContent;
-        console.log('Category: ', textTitleElement);
-    
-        const numberLiItems = elem.children[1].children;
-        console.log('Elements: ', numberLiItems.length);
-    }
-
+        console.log('Category: ', titleElement);
+        console.log('Elements: ', numberItems.length);
+    })
 };
 
-countQuantityElementsCategoryWithTitleAndLength();
+findElementsUl();
